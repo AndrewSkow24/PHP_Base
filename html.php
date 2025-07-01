3cbfbf64a4ec9081b30aeb82c081bb45b9a2f9ca
Andrew Skow <anjeie24@mail.ru>
2025-07-01 13:56:14 +0300

    26of402

---
+++ html.php
@@ -0,0 +1,21 @@
+<?php
+
+function h1($header_text)
+{
+    echo "<h1>$header_text</h1>";
+}
+
+function h2($header_text)
+{
+    echo "<h2>$header_text</h2>";
+}
+
+function p($paragraph_text)
+{
+    echo "<p>$paragraph_text</p>";
+}
+
+function task_heading($task_number)
+{
+    echo "<h2>Задача $task_number</h2>";
+}
