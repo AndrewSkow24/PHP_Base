ecdc16f4f3cd3db4b98b51d71d234d163e785e45
Andrew Skow <anjeie24@mail.ru>
2025-07-03 15:00:03 +0300

    42of402

---
+++ 041.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'html.php';
+
+// можно также преобразовать число к строке 
+
+$test = (string) 123;
+var_dump($test);
+br();
+
+// аналогичным образом будет работать преобразование дробного числа
+$test = (string) 1.2;
+var_dump($test);
+br();
+
+task_heading(1);
+p("Преобразуйте следующее число в строку:");
+
+$test = 12345;
+$test = (string) $test;
+var_dump($test);
+
+task_heading(2);
+p("Преобразуйте следующее число в строку:");
+$test = 12.345;
+$test = (string) $test;
+var_dump($test);
